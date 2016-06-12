24ca6eb2fccea349f4b3280a446c2726c9013ae5
hongxiang <916634913@qq.com>
2016-06-12 15:57:10 +0800

    邹良玉2

--- MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     //HongXiang
 
     // 鸿翔你是不是傻
+    //邹良玉简直就是个四，除了二还是二，减去二还是二，真是二上加二，去掉一个二还有一个二，而且是二的二倍。
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
