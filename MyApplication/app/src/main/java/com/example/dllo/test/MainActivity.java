a0fac32999666521f7880521d3b72b18df08457e
张锡威 <chshkfb@163.com>
2016-06-12 16:01:14 +0800

    你俩是不是啥

--- MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
@@ -14,11 +14,14 @@ public class MainActivity extends AppCompatActivity {
 //邹良玉百度搜不到你，进搜狗一下字就搜到你了。我妈说了,我在玩游戏的时候,谁要是打扰我,就给他灌瓶妇炎洁。你的牙如同天上的繁星，色泽鲜艳，相距甚远。真想立马把我37码的鞋穿到你42码的脸上。
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.
+                onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // 鸿翔笨死了的
         // 鸿翔我了个擦
-        // 哪里来的, 
+        // 哪里来的,
+
+        //你俩是不是傻
     }
 }
