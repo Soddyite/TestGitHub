a2eabe023ad005ccf7fa67b2e183aea6f02cadd4
hongxiang <916634913@qq.com>
2016-06-12 15:58:06 +0800

    邹良玉3

--- MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     // 鸿翔你是不是傻
     //邹良玉简直就是个四，除了二还是二，减去二还是二，真是二上加二，去掉一个二还有一个二，而且是二的二倍。
-
+//邹良玉百度搜不到你，进搜狗一下字就搜到你了。我妈说了,我在玩游戏的时候,谁要是打扰我,就给他灌瓶妇炎洁。你的牙如同天上的繁星，色泽鲜艳，相距甚远。真想立马把我37码的鞋穿到你42码的脸上。
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
