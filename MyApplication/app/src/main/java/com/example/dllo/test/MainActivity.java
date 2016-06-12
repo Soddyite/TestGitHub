b62e13fc464324b8d28685320fc055ab2088c2ab
hongxiang <916634913@qq.com>
2016-06-12 15:53:27 +0800

    HongXiang

--- MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dllo/test/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //hehe
+    //HongXiang
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
